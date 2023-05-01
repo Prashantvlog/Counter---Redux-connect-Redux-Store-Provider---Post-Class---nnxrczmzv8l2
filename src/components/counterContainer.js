66649258da2b200d6af9d44fdf62b3683fa58872
Prashant Sonwane <prashants0209@gmail.com>
2023-05-01 12:41:33 +0530

    Update counterContainer.js

--- src/components/counterContainer.js
+++ src/components/counterContainer.js
@@ -3,16 +3,22 @@ import Counter from "./counter";
 import { increment, decrement, reset } from "../actions";
 
 const mapStateToProps = (state) => {
-    return {
-        counter: state,
-    };
-};
-const mapDispatchToProps = (dispatch) => {
-    return {
-        increment: () => dispatch(increment()),
-        decrement: () => dispatch(decrement()),
-        reset: () => dispatch(reset()),
-    };
+  return {
+    counter: state,
+  };
 };
 
-// add code here
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement()),
+    reset: () => dispatch(reset()),
+  };
+};
+
+const CounterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+export default CounterContainer;
